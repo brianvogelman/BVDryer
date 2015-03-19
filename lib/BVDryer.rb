ff511ebef0247141374748cf3660c2f92e13ea10
Brian Vogelman <bvogelman7@gmail.com>
2015-03-19 18:42:53 -0500

    initial commit

---
+++ lib/BVDryer.rb
@@ -0,0 +1,9 @@
+require "BVDryer/version"
+
+module BVDryer
+  class Dryer
+	def dry
+		'say so dry oooooooooooooooooooooooo oooooooooooooooooooooooo'
+	end
+  end
+end
---
+++ lib/BVDryer/version.rb
@@ -0,0 +1,3 @@
+module BVDryer
+  VERSION = "0.1.0"
+end
